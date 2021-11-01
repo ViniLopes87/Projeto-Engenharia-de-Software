4bab185f9da98c81d867ebe46ee1a213b78e69a2
João Vitor Ângelo Pereira <jvangelo172019@gmail.com>
2021-11-01 08:44:51 -0300

    Implementacao metodos de Controle da lista de alunos

---
+++ javascript/ControleAlunos.js
@@ -0,0 +1,14 @@
+const listaalunos
+
+function exibirlista(){
+//exibir a lista
+}
+function procurarAluno(){
+//procurar e exibir o aluno
+}
+function filtrarAlunos(){
+//filtrar alunos por nome,status ou periodo    
+}
+function selecionarAluno(){
+//Selecionar alunos para mais informacoes    
+}
