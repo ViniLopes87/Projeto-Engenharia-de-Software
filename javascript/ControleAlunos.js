c1610616aa1617bfbddeabdefde6c62274ffb5df
João Vitor Ângelo Pereira <jvangelo172019@gmail.com>
2021-11-03 18:13:14 -0300

    Alteracao no metodo exibirLista

--- javascript/ControleAlunos.js
+++ javascript/ControleAlunos.js
@@ -1,18 +1,47 @@
-localStorage.getItem("lista_alunos")
-alunos = localStorage.getItem("lista_alunos").split(",")
+let cpf = document.getElementById("cpf");
+const lista = document.getElementById("lista");//Por o ID da lista que Arthur irá criar na pagina 
+var result = [];
+
 function exibirlista(){
-    var i = 1
-    var result = []
+alunos = localStorage.getItem("lista_alunos").split(",");
+    var i = 0;
     for( ; i<=alunos.length; i++){
-        aluno = JSON.parse(localStorage.getItem(alunos[i]))
-        result.push(aluno)
+        aluno = JSON.parse(localStorage.getItem(alunos[i]));
+        result.push(aluno);
     }
-    return result
+     exibirAlunos();
 }
+function exibirAlunos() {
+    lista.innerHTML = "";
+    result.forEach(result => {
+      const newLi = document.createElement("li");
+      const newText = document.createTextNode(result.nome);
+      newLi.appendChild(newText);
+      newLi.appendChild(result.cpf);
+      lista.appendChild(newLi);
+    });
+  }
+  
+
 function procurarAluno(){
-    aluno = JSON.parse(localStorage.getItem(""))
-    return aluno
+    if(cpf.value == " "){
+       alert("Insira algo no campo CPF.")
+    }
+    else if(cpf.value.length<11||cpf.value.length>11){
+            alert("Insira um número de CPF com tamanho valido.")
+    }else{
+       aluno = JSON.parse(localStorage.getItem(cpf))
+       if(aluno==null){
+          alert("CPF do aluno não encontrado!");
+       }else{
+           return aluno
+       }
 }
+
 function selecionarAluno(){
    
 }
+
+}
+exibirlista();
+
