d62d98cf4f78b5a1f5fb4f8851e15c01fec3534c
Vinícius Lopes da Silva <viniciuslopes_2000@hotmail.com>
2021-11-17 09:35:17 -0300

    Criando o menu para o suporte

---
+++ Menu/Menu-Suporte/app.js
@@ -0,0 +1,5 @@
+document.querySelector(".close").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
+
+window.alert("O suporte possui ${n} sugestões/reclamações na lista");
