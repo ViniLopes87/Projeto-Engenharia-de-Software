e4f828798fdea5c9f593aaeac52958e291d9c997
João Vitor Ângelo Pereira <jvangelo172019@gmail.com>
2021-11-18 19:04:10 -0300

    atualização da branch e metodo selecionarAluno

---
+++ student-view/data-student.js
@@ -0,0 +1,8 @@
+nome = document.getElementById('nome');
+const selecionado;
+
+function AlunoSelecionado(aluno){
+   selecionado = aluno;
+}
+
+nome.innerText = selecionado.nome
--- student-view/student.js
+++ student-view/student.js
@@ -1,9 +1,9 @@
+ //export function AlunoSelecionado() {};
+ //import {AlunoSelecionado} from 'data-student.js';
  result = []
- //resultOne = []
- //const lista = document.getElementById('list')
  const filter = document.getElementById('filtrar')
  const btFiltrar = document.getElementById("btFiltrar")
- //const response = document.getElementById("resp")
+
 function studentView() {
   window.location.href = ".student-view/student-view.html";
 }
@@ -35,18 +35,12 @@ function exibirAlunos() {
       td_status.innerText = result[i].status;
       
       let imgInfo = document.createElement('info');
-      imgInfo.innerText = "(+)"
+      imgInfo.innerText = "(+)";
+      imgInfo.setAttribute("onclick","ExibirDados("+result[i].cpf+")");
       td_info.appendChild(imgInfo);
-      imgInfo.setAttribute("onclick","teste()")
+      
   } 
-  //lista.innerHTML = "";
-  //result.forEach((result) => { if(result!=null){
-    //const newLi = document.createElement("li");
-    //const newText = document.createTextNode(result.nome + " - " + result.matricula +" - " + result.periodo + " - " + result.status);
-    //newLi.appendChild(newText);
-    //lista.appendChild(newLi);
-  //}
-  //});
+
 }
 function filtrarAluno() {
   let tbody2 = document.getElementById('tbody2');
@@ -71,13 +65,9 @@ function filtrarAluno() {
       
   
       let imgInfo = document.createElement('info');
-      imgInfo.innerText = "(+)"
+      imgInfo.innerText = "(+)";
+      imgInfo.setAttribute("onclick","ExibirDados("+ aluno.cpf +")");
       td_info.appendChild(imgInfo);
-      imgInfo.setAttribute("onclick","teste()")
-      
-      //const newText = aluno.nome + " - " + aluno.matricula +" - " + aluno.periodo + " - " + aluno.status;
-      //response.innerHTML = newText;
-      //tbody.style.display = "none"
   
     }else{
       alert('CPF do aluno não encontrado')
@@ -85,14 +75,63 @@ function filtrarAluno() {
   }else{
      alert('Insira um CPF no campo')
   }
-  
-  
-  
 }
-function teste(){
-  alert('teste')
+
+function ExibirDados(Cpf){
+  divtb = document.getElementById('divtb');
+  aluno = JSON.parse(localStorage.getItem(Cpf));
+  if(aluno!=null){
+    divtb.style.width = "55rem"
+    divtb.style.left = "20%"
+    tb1 = document.getElementById('tb1');
+    tb2 = document.getElementById('tb2');
+    tbody3 = document.getElementById('tbody3');
+    tbody3.innerText = "";
+    tb1.style.display = 'none';
+    tb2.style.display = 'block';
+    
+    let tr = tbody3.insertRow();
+  
+    let td_nome = tr.insertCell();
+    let td_matricula = tr.insertCell();
+    let td_periodo = tr.insertCell();
+    let td_status = tr.insertCell();
+    let td_cpf = tr.insertCell();
+    let td_email = tr.insertCell();
+    let td_curso = tr.insertCell();
+    let td_historico = tr.insertCell();
+    let td_avaliacao = tr.insertCell();
+    let td_voltainfo = tr.insertCell();
+  
+    td_nome.innerText = aluno.nome;
+    td_matricula.innerText = aluno.matricula;
+    td_periodo.innerText = aluno.periodo+"º";
+    td_status.innerText = aluno.status;
+    td_cpf.innerText = aluno.cpf;
+    td_email.innerText = aluno.email;
+    td_curso.innerText = aluno.curso;
+    td_historico.innerText = aluno.historico;
+    td_avaliacao.innerText = aluno.avaliacao;
+  
+    let imgInfo = document.createElement('info');
+    imgInfo.innerText = "(-)";
+    imgInfo.setAttribute("onclick","voltar()");
+    td_voltainfo.appendChild(imgInfo); 
+  }else{
+    alert('erro');
+  }
 }
-  btFiltrar.onclick = filtrarAluno;
-  exibirlista();
+function voltar(){
+  tb1 = document.getElementById('tb1');
+  tb2 = document.getElementById('tb2');
+  divtb = document.getElementById('divtb');
+  divtb.style.width = "30rem"
+  divtb.style.left = "40%"
+  tb1.style.display = 'block';
+  tb2.style.display = 'none';
+}
+
+btFiltrar.onclick = filtrarAluno;
+exibirlista();
 
 
