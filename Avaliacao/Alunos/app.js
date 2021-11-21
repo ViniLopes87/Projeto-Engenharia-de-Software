86018e9af363c4606f69de4a6b1437e80bfa1512
Raffael Thobias <dashp@outlook.com.br>
2021-11-21 15:03:14 -0300

    Avaliacao aluno

--- Avaliacao/Alunos/app.js
+++ Avaliacao/Alunos/app.js
@@ -3,4 +3,11 @@ function handleForm(event) { event.preventDefault(); }
 form3.addEventListener('submit', handleForm);
 
 
-
+function SendAvaliacao() {
+    const tipouser = sessionStorage.getItem("logado").split(",");
+const aluno = localStorage.getItem(tipouser[1]);
+const avaliacaotexto = document.getElementById("areatexto")
+const avaliacaostar = document.getElementById("estrelas")
+alert(avaliacaotexto);
+    alert("Avaliação enviada!");
+  } 
