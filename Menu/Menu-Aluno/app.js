524adebcb352d55b69fc5ee5324e71c8d91933db
Raffael Thobias <dashp@outlook.com.br>
2021-11-21 17:19:41 -0300

    Alerta de pendências do menu aluno

--- Menu/Menu-Aluno/app.js
+++ Menu/Menu-Aluno/app.js
@@ -1,3 +1,10 @@
 document.querySelector(".close").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+if (!localStorage.getItem("lista_pendencia") == "") {
+    const lista = localStorage.getItem("lista_pendencia").split(",");
+    window.alert(
+      "Você possui " + lista.length + " Pendências."
+    );
+  }
+  
