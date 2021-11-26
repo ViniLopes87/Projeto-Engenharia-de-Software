c8d89dd83e666d9dc7081f7d96fe6ad69684053e
Vinícius Lopes da Silva <viniciuslopes_2000@hotmail.com>
2021-11-26 00:25:00 -0300

    Modificando o layout das paginas de avaliacoes para manter o padrao

--- Avaliacao/Alunos/app.js
+++ Avaliacao/Alunos/app.js
@@ -1,3 +1,9 @@
+document
+  .querySelector(".close")
+  .addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+  );
+
 var form3= document.getElementById("formcad");
 function handleForm(event) { event.preventDefault(); } 
 form3.addEventListener('submit', handleForm);
@@ -10,4 +16,4 @@ const avaliacaotexto = document.getElementById("areatexto")
 const avaliacaostar = document.getElementById("estrelas")
 alert(avaliacaotexto);
     alert("Avaliação enviada!");
-  } 
+}
--- Avaliacao/Empresas/app.js
+++ Avaliacao/Empresas/app.js
@@ -1,6 +1,9 @@
+document
+  .querySelector(".close")
+  .addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+  );
+
 var form3= document.getElementById("formcad");
 function handleForm(event) { event.preventDefault(); } 
 form3.addEventListener('submit', handleForm);
-
-
-
