cb829bab5ab212931d5f16349c81cfb908c48b58
arthurlba1 <1arthurluiz1@gmail.com>
2021-12-01 18:09:00 -0300

    remove student pendency

--- javascript/ControlePendencias.js
+++ javascript/ControlePendencias.js
@@ -1,5 +1,4 @@
 cont=1;
-console.log("inicial"+cont)
 function sendPendencia() {
     qtd = Math.random() * (1000 - 1) + 1;
     const InfoGeral = {
@@ -24,7 +23,6 @@ function sendPendencia() {
           );
       }
     } else {
-        console.log("um"+cont)
       localStorage.setItem("id:" + InfoGeral.matricula+","+cont, JSON.stringify(InfoGeral));
       localStorage.setItem("lista_pendencia", InfoGeral.matricula);
     }
@@ -45,11 +43,17 @@ function exibePendencia() {
     result = [];
     sugestoes = localStorage.getItem("lista_pendencia").split(",");
     for (i; i <= sugestoes.length; i++) {
-        console.log(aluno.matricula)
         if(aluno.matricula==sugestoes[i]){
             for(j=0; j<10; j++){
                 if(localStorage.getItem("id:" + sugestoes[i]+","+j)){
                     sugestoesF = JSON.parse(localStorage.getItem("id:" + sugestoes[i]+","+j));
+                    sugestoesF = {
+                      matricula: sugestoesF.matricula,
+                      nome: sugestoesF.nome,
+                      prazo: sugestoesF.prazo,
+                      Texp: sugestoesF.Texp,
+                      numero: j
+                    }
                     result.push(sugestoesF);
                 }
             }
@@ -62,12 +66,57 @@ function exibePendencia() {
         menulista.appendChild(newTr);
         const newTh = document.createElement("th");
         const newTh2 = document.createElement("th2");
+        const newTh3 = document.createElement("th3")
+        const newTh4 = document.createElement("th4")
         const text = document.createTextNode(result.Texp);
         const data = document.createTextNode(result.prazo)
+        const file = document.createElement("input")
+        file.setAttribute("type","file")
+        const button = document.createElement("button")
+        button.setAttribute(
+          "onclick",
+          "deletarPendencia(" + result.matricula+","+result.numero+")"
+        );
         newTh.appendChild(text);
         newTh2.appendChild(data);
+        newTh3.appendChild(file);
+        newTh4.appendChild(button);
         newTr.appendChild(newTh);
         newTr.appendChild(newTh2);
+        newTr.appendChild(newTh3)
+        newTr.appendChild(newTh4)
       }
     });
+  }
+
+  function deletarPendencia(matricula, numero) {
+    localStorage.removeItem("id:" + matricula+","+numero);
+    sugestoes = localStorage.getItem("lista_pendencia").split(",");
+    cont = 0;
+    for (i = 0; i <= sugestoes.length; i++) {
+      if(matricula==sugestoes[i]){
+          for(j=0; j<10; j++){
+            if(localStorage.getItem("id:" + sugestoes[i]+","+j)){
+              cont++
+              alert(cont)
+            }
+          }
+      }
+    }
+    if(cont!=1){
+      localStorage.removeItem("lista_pendencia");
+      for (j = 0; j <= sugestoes.length; j++) {
+        if (sugestoes[j] != matricula && sugestoes[j] != null) {
+          if (j === 0) {
+            localStorage.setItem("lista_pendencia", sugestoes[j]);
+          } else {
+            localStorage.setItem(
+              "lista_pendencia",
+              localStorage.getItem("lista_pendencia") + "," + sugestoes[j]
+            );
+          }
+        }
+      }
+    }
+    window.location.reload();
   }
